70d6654b10467d6a3e1b8207ca1545eb0b1b624f
CHAPPIDI-GOPI <gopichintu.483@gmail.com>
2020-12-28 23:53:25 +0530

    operator_overloading

---
+++ operator_overloading.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+class OperatorOverloading
+{
+	public:
+	int count;
+		OperatorOverloading(){count = 5 ;}
+		OperatorOverloading(int c)
+		{
+			count = c;
+		}
+		void operator ++()
+		{
+			count = count + 1;
+		}
+		void operator --()
+		{
+			count = count - 1;
+		}
+		OperatorOverloading operator + (OperatorOverloading &ob2)
+		{
+			OperatorOverloading ob;
+			ob.count = count + ob2.count;
+		}
+		OperatorOverloading operator - (OperatorOverloading &ob2)
+		{
+			OperatorOverloading ob;
+			ob.count = count - ob2.count;
+		}
+		void display()
+		{
+			cout<<"count : "<<count<<endl;
+		}
+};
+//int OperatorOverloading::count= 5;
+class assignment
+{
+	public:
+		int *ptr;
+		assignment(){}
+		assignment(int i)
+		{
+			ptr = new int(i);
+		}
+		assignment & operator = (assignment &ob3)
+		{
+			if(this!= &ob3)
+				*ptr = *(ob3.ptr);
+			return *this;
+		}
+		void display()
+		{
+			cout<<"*ptr :"<<*ptr<<endl;
+		}
+};
+int main()
+{
+	OperatorOverloading obj;
+	obj.display();
+	--obj;
+	obj.display();
+	++obj;
+	obj.display();
+	OperatorOverloading obj1(4);
+	OperatorOverloading obj2(2);
+	OperatorOverloading obj3;// = obj1 + obj2;
+	obj3 = obj1 + obj2;
+	obj3.display();
+	OperatorOverloading obj4;//  = obj1 - obj2;
+	obj4 = obj1 - obj2;
+	obj4.display();
+	OperatorOverloading obj5;//  = obj1 - obj2;
+	obj5 = obj4;
+	obj5.display();
+	assignment object1(6);
+	assignment object2;// = object1;
+	object2 = object1;
+	object2.display();
+
+	return 0;
+}
